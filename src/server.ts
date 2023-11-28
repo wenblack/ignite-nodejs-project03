821de61915b06c40521e2b15443a147239d80502
Wen Barbosa <wenblack012@gmail.com>
2023-11-28 14:42:28 -0300

    🔧 build: Locked dependencies versions

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 const PORT = 3333
 
 app.listen({
-  host: '0.0.0.0',
+  host: '0.0.0.0',// hack to  able connection from front-end 
   port: PORT,
 }).then(() => {
   console.log(`🚀 HTTP Server Running on port ${PORT}!`)
